319ef81cb98c6b73418e3bf1836c151485e25f34
HusseinSaeid <ghost,sehs@gmail.com>
2026-05-30 20:03:16 +0300

    small fix

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client";
 import ForensicsForm from "@/components/forensicsForm";
+import { Steps } from "@/components/steps";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 bg-[#1e1e1e] justify-center items-center p-12 gap-12 ">
       <h1 className="text-red-700 text-4xl">PlaceHolder</h1>
       <ForensicsForm />
+      <Steps />
     </div>
   );
 }
