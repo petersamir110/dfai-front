da14ce6775731dcc799b894ed59fe3de44b2f660
SEHS <55005651+HusseinSaeid@users.noreply.github.com>
2026-08-28 20:04:50 +0300

    Remove comments from NavBar component
    Removed comments for logo and project name sections in the NavBar component.

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -7,7 +7,6 @@ export default function NavBar() {
   const { projectTitle } = useProject();
   return (
     <div className="sticky top-0 z-50 h-14 p-2 border-b-2 bg-[#03060C] border-[#303439]/40 flex items-center justify-between w-full">
-      {/* DFAI Logo */}
       <div className="flex items-center">
         <Image
           src="/images/side-bar-logo.png"
@@ -18,14 +17,12 @@ export default function NavBar() {
         />
       </div>
 
-      {/* ProjectName #Work in Progress */}
       <div className="p-2 bg-[#080d1a] rounded-lg w-96 flex items-center justify-center border border-[#303439]/60 h-9">
         <h1 className="pl-3 text-gray-300 font-medium layout-title capitalize text-2xl">
           {projectTitle === "" ? "New Project" : projectTitle}
         </h1>
       </div>
 
-      {/* Other Logos */}
       <div className="flex flex-row items-center gap-1">
         <Image
           src="/images/Digilians.png"
